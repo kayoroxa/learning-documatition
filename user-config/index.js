d7ed08c4b1fa1467bc85d446cba5425840c63d67
herbert366 <herbert.artur18@gmail.com>
2022-07-16 14:13:21 -0300

    theme

--- user-config/index.js
+++ user-config/index.js
@@ -7,9 +7,15 @@ function findUserData(userLogin) {
 
 const userData = findUserData(userLogin)
 
+function userTheme(userData) {
+  if (userData.userTheme === 'dark') {
+    return '🌑'.repeat(50)
+  }
+  if (userdata.userTheme === 'light') {
+    return '🌞'
+  } else "you don't have a theme "
+}
 console.log(`your password is: ${userData.userPassword}`)
 console.log(`your age is: ${userData.userAge}`)
 console.log(`your userName is: ${userData.userName}`)
-console.log(
-  `your theme is: ${userData.userTheme} look how pretty it is: °°-V-°°!`
-)
+console.log(userTheme(userData))
