2d762e6782a0b5288a68b33ba82173f0c9df18bd
Caio <kayoroxa@gmail.com>
2022-07-18 19:56:24 -0300

    .

--- draw svg/index.js
+++ draw svg/index.js
@@ -1,13 +1,33 @@
 const svgPath = document.querySelectorAll('path')
 
-const svgText = anime({
-  targets: svgPath,
-  loop: true,
-  direction: 'alternate',
-  strokeDashoffset: [anime.setDashoffset, 0],
-  easing: 'easeInOutSine',
-  duration: 700,
-  delay: (el, i) => {
-    return i * 500
-  },
+const handAlongPath = document.querySelector('.hand')
+
+var tl = anime.timeline({
+  easing: 'linear',
+  duration: 30000,
+})
+
+handAlongPath.style.transform = `translate(1000px, 1000px)`
+
+svgPath.forEach(path => {
+  tl.add(
+    {
+      targets: path,
+      strokeDashoffset: [anime.setDashoffset, 0],
+      easing: 'easeInOutSine',
+      // duration: 1000,
+      delay: anime.stagger(100),
+
+      change: function (anim) {
+        // hand along path
+        const path = anim.animations[0].animatable.target
+        if (!path) return
+        const { x, y } = path.getPointAtLength(
+          (anim.progress / 100) * path.getTotalLength()
+        )
+        handAlongPath.style.transform = `translate(${x + 280}px, ${y - 163}px)`
+      },
+    },
+    '+=10'
+  )
 })
