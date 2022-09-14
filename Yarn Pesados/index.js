de84ba9b3253fbf5ca24fffa69ec37241a46c021
herbert366 <herbert.artur18@gmail.com>
2022-09-14 17:55:58 -0300

    nem lembro oq foi

--- Yarn Pesados/index.js
+++ Yarn Pesados/index.js
@@ -1,13 +1,31 @@
-var bnt = document.querySelector('.bnt')
+var bntFocusInVideo = document.querySelector('.bnt-focus-in-video')
+var bntReset = document.querySelector('.bnt-reset')
 var containerSuper = document.querySelector('.container-superior-All')
-var videoSixText = document.querySelector('#video-6-subtitle')
-var videSixVideo = document.querySelector('#video-6')
+var containerVideo = document.querySelector('.video-container-6')
 
-bnt.addEventListener('click', function () {
-  videoSixText.style.backgroundColor = 'red'
-  var videoCloned = videSixVideo.cloneNode(true)
-  containerSuper.appendChild(videoCloned)
-  containerSuper.appendChild(videoSixText)
+// bntFocusInVideo.addEventListener('click', function focus() {
+//   var cloneComp = containerVideo.cloneNode(true)
+//   cloneComp.setAttribute('id', 'Video-Focus')
+
+//   if (containerSuper.classList.contains('hide')) {
+//     containerSuper.classList.remove('hide')
+//     containerSuper.appendChild(cloneComp)
+//   } else containerSuper.classList.add('hide')
+
+//   if (containerSuper.childNodes.length > 4) {
+//     containerSuper.removeChild(cloneComp)
+//   }
+// })
+
+bntReset.addEventListener('click', function reset() {
+  containerSuper.classList.add('hide')
+  var getClass = this.classList[1]
+  var ale = getClass
+  console.log(typeof ale, getClass)
 })
 
-console.log(videSixVideo.childNodes)
+bntFocusInVideo.addEventListener('click', function focus() {
+  var getClass = this.classList[2]
+  var classString = toString(getClass)
+  console.log(getClass, typeof classString, classString)
+})
