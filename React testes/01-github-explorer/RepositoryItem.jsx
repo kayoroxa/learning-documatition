255fd3d579c179a723749db462f7016737fd4171
herbert366 <herbert.artur18@gmail.com>
2022-09-19 23:30:27 -0300

    primeiros passos no react

---
+++ React testes/01-github-explorer/RepositoryItem.jsx
@@ -0,0 +1,9 @@
+export function RepositoryItem() {
+  return (
+    <li>
+      <strong>Uniform</strong>
+      <p>Forms In React</p>
+      <a href="google.com">Acessar Repositório</a>
+    </li>
+  )
+}
